abbdb5dfb51865eb301a8b483b48353b4eaf12a4
Dev-Everly <everlysantos98@gmail.com>
2026-06-12 11:07:12 -0300

    aula_02 e exercicio prático 01

---
+++ aula_02_operadores/src/aula_02_operadores/OperadoresUnarios.java
@@ -0,0 +1,22 @@
+package aula_02_operadores;
+
+public class OperadoresUnarios {
+
+	public static void main(String[] args) {
+			
+		//Operadores Unarios Operam somente em um elemento
+		
+		
+		int contador = 1000;
+		
+		System.out.println("Valor inicial do contador: " + contador);
+		
+		//System.out.println("Valor Pré Incrementado: " + (++ contador));
+		
+		System.out.println("Valor Pós Incremento: " + (contador ++ ));
+		
+		System.out.println("Novo Valor de Contador: " + contador);
+		
+	}
+
+}
---
+++ aula_02_operadores/src/exercicios_aula02/exec_01.java
@@ -0,0 +1,30 @@
+package exercicios_aula02;
+
+import java.util.Scanner;
+
+public class exec_01 {
+
+	public static void main(String[] args) {
+		
+		 
+		 
+		Scanner leia = new Scanner(System.in);
+		System.out.println("Digite o salário: ");
+		double salario = leia.nextDouble();
+		
+		System.out.println("Digite o abono : ");
+		double abono = leia.nextDouble();
+		
+		double novoSalario = salario + abono;
+		
+		System.out.printf("Novo Salário %.2f", novoSalario);
+		
+		
+		
+		
+		
+		
+ 
+	}
+
+}
